2706b77628554fec3c08a94dc211ec8b08e2956b
Asma-Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2022-05-11 12:25:47 -0900

    Update Getting_In_Shape.py

--- Getting_In_Shape.py
+++ Getting_In_Shape.py
@@ -1,4 +1,9 @@
-file = open("/usercode/files/pull_ups.txt")
+'''
+Tom has done pull ups every day and recorded his results.
+He recorded each day's results in a new line, so that each line represents each day he has done pull ups.
+Create a program that takes n number as input and outputs the n-th days result (starting from 0).
+
+'''file = open("/usercode/files/pull_ups.txt")
 n = int(input())
 list= file.readlines()
 #your code goes here
