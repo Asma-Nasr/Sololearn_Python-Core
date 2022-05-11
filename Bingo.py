e690f4a460cce306d1eb553d4c493cf897fd7d67
Asma-Nasr <78988623+Asma-Nasr@users.noreply.github.com>
2022-05-11 12:21:24 -0900

    Update Bingo.py

--- Bingo.py
+++ Bingo.py
@@ -1,3 +1,8 @@
+'''Given a list of numbers, output "bingo" if it contains the input number.
+and Do not output anything if the number is not found
+'''
+
+
 items = [42, 88, 721, 12, 43, 22, 908]
 
 num = int(input())
